45cb48c696ae50d0fd6bd2cedcfac62f9319b32f
Morgan Pizzini <morgan.pizzini@hotmail.com>
2019-04-06 20:34:53 +0200

    fix inline editing template

--- projects/potara-lib/src/lib/components/inline-edit/inline-edit.component.ts
+++ projects/potara-lib/src/lib/components/inline-edit/inline-edit.component.ts
@@ -1,72 +1,65 @@
 import {
+  ChangeDetectionStrategy,
   Component,
-  Input,
   ElementRef,
-  ViewChild,
-  Renderer,
   forwardRef,
+  Input,
   OnInit,
-  ChangeDetectorRef
+  ViewChild
 } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-const INLINE_EDIT_CONTROL_VALUE_ACCESSOR = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => InlineEditComponent),
-  multi: true
-};
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'pot-inline-edit',
   templateUrl: './inline-edit.component.html',
-  providers: [INLINE_EDIT_CONTROL_VALUE_ACCESSOR],
-  styleUrls: ['./inline-edit.component.scss']
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InlineEditComponent), multi: true }
+  ],
+  styleUrls: ['./inline-edit.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class InlineEditComponent implements ControlValueAccessor, OnInit {
-
   @ViewChild('inlineEditControl') inlineEditControl: ElementRef; // input DOM element
-  @Input() label = '';  // Label value for input element
+  @Input() label = ''; // Label value for input element
   @Input() type = 'text'; // The type of input element
   @Input() required = false; // Is input requried?
   @Input() disabled = false; // Is input disabled?
   @Input() saveOnBlur = false; // Is input disabled?
-  private _value = ''; // Private variable for input value
   private preValue = ''; // The value before clicking to edit
   editing = false; // Is Component in edit mode?
-  public onChange: any = Function.prototype; // Trascend the onChange event
-  public onTouched: any = Function.prototype; // Trascend the onTouch event
 
-  // Control Value Accessors for ngModel
-  get value(): any {
-    return this._value;
+  // tslint:disable-next-line: no-input-rename
+  @Input('inputValue') _inputValue: string;
+
+  get inputValue() {
+    return this._inputValue;
   }
 
-  set value(v: any) {
-    if (v !== this._value) {
-      this._value = v;
-      this.onChange(v);
-      // this.changeDetector.detectChanges();
+  set inputValue(val) {
+    this._inputValue = val;
+    this.propagateChange(val);
+  }
+
+  propagateChange: any = () => {};
+  validateFn: any = () => {};
+
+  writeValue(value) {
+    if (value) {
+      this.inputValue = value;
     }
   }
 
-  constructor(private changeDetector: ChangeDetectorRef) {
+  registerOnChange(fn) {
+    this.propagateChange = fn;
   }
 
-  // Required for ControlValueAccessor interface
-  writeValue(value: any) {
-    this._value = value;
+  registerOnTouched() {}
+
+  validate(c: FormControl) {
+    return this.validateFn(c);
   }
 
-  // Required forControlValueAccessor interface
-  public registerOnChange(fn: (_: any) => {}): void {
-    this.onChange = fn;
-  }
-
-  // Required forControlValueAccessor interface
-  public registerOnTouched(fn: () => {}): void {
-    this.onTouched = fn;
-  }
+  constructor() {}
 
   // Do stuff when the input element loses focus
   onBlur($event: Event) {
@@ -77,16 +70,15 @@ export class InlineEditComponent implements ControlValueAccessor, OnInit {
   }
 
   // Start the editting process for the input element
-  edit(value) {
+  edit() {
     if (this.disabled) {
       return;
     }
 
-    this.preValue = value;
+    this.preValue = this._inputValue;
     this.editing = true;
     // Focus on the input element just as the editing begins
     // force layout update
-    this.changeDetector.detectChanges();
 
     this.inlineEditControl.nativeElement.focus();
     // setTimeout(_ => this._renderer.invokeElementMethod(this.inlineEditControl,
@@ -94,15 +86,12 @@ export class InlineEditComponent implements ControlValueAccessor, OnInit {
   }
   save() {
     this.editing = false;
-    this.changeDetector.detectChanges();
   }
 
   abort() {
-    this.value = this.preValue;
+    this.inputValue = this.preValue;
     this.editing = false;
-    this.changeDetector.detectChanges();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
--- projects/potara-lib/src/lib/services/common/http-client-base.ts
+++ projects/potara-lib/src/lib/services/common/http-client-base.ts
@@ -1,64 +1,65 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 
-
 /**
  * Base abstract class for HTTP service
  */
 export abstract class HttpClientBase {
+  constructor(private httpClient: HttpClient, private apiBaseUrl: string) {}
 
-    constructor(
-        private httpClient: HttpClient,
-        private apiBaseUrl: string) {
-    }
+  // url regular expression check
+  private urlRegExp = new RegExp(
+    '^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$'
+  );
 
-    /**
-     * Starts an HTTP invoke on remote service
-     * @param method Method
-     * @param partialUrl Partial url
-     * @param body Body of request
-     * @param headers Custom headers
-     */
-    protected invoke<TResponse>(method: string, partialUrl: string, body: object): Observable<TResponse> {
+  /**
+   * Starts an HTTP invoke on remote service
+   * @param method Method
+   * @param partialUrl Partial url
+   * @param body Body of request
+   * @param headers Custom headers
+   */
+  protected invoke<TResponse>(
+    method: string,
+    partialUrl: string,
+    body: object
+  ): Observable<TResponse> {
+    // Create observable source
+    const observableSource = new Subject<TResponse>();
 
-        // Create observable source
-        const observableSource = new Subject<TResponse>();
+    // Declare observable
+    const observable: Observable<any> = observableSource.asObservable();
 
-        // Declare observable
-        const observable: Observable<any> = observableSource.asObservable();
+    // Compose full url
+    // check for partial as full url
+    const fullUrl = this.urlRegExp.test(partialUrl) ? partialUrl : this.apiBaseUrl + partialUrl;
 
-        // Compose full url
-        const fullUrl = this.apiBaseUrl + partialUrl;
+    // Compose options
+    const options = {
+      body: !body ? null : JSON.stringify(body)
+    };
 
-        // Compose options
-        const options = {
-            body: !body ? null : JSON.stringify(body),
-        };
+    // Invoke remote method
+    this.httpClient.request(method, fullUrl, options).subscribe(
+      // Success
+      (response: any) => {
+        // Convert data to target type
+        const data: TResponse = response;
 
-        // Invoke remote method
-        this.httpClient.request(method, fullUrl, options).subscribe(
+        // Apply error on observable and complete
+        observableSource.next(data);
+        observableSource.complete();
+      },
 
-            // Success
-            (response: any) => {
+      // Error
+      (error: any) => {
+        // Apply error on observable and complete
+        observableSource.error(error);
+        observableSource.complete();
+      }
+    );
 
-                // Convert data to target type
-                const data: TResponse = response;
-
-                // Apply error on observable and complete
-                observableSource.next(data);
-                observableSource.complete();
-            },
-
-            // Error
-            (error: any) => {
-
-                // Apply error on observable and complete
-                observableSource.error(error);
-                observableSource.complete();
-            }
-        );
-
-        // Returns observable
-        return observable;
-    }
+    // Returns observable
+    return observable;
+  }
 }
